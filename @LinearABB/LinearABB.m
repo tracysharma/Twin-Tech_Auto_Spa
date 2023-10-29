dfdf2ba94044bb52f60031f6db43c5033bbf7206
ElisaZ <73983562+elissssaz@users.noreply.github.com>
2023-10-29 21:29:13 +1100

    Finalised ABB
    Working

--- @LinearABB/LinearABB.m
+++ @LinearABB/LinearABB.m
@@ -23,28 +23,16 @@ function self = LinearABB(baseTr)
             % Create the UR5 model mounted on a linear rail
             link(1) = Link([pi     0       0       pi/2    1]); % PRISMATIC Link - linear rail
 %%ABB-IRB-2600/12's dh parameters ADDED
-            link(2) = Link('d',0.455,'a',0.150,'alpha',-pi/2,'qlim',deg2rad([-360 360]));
-            link(3) = Link('d',0,'a',0.900,'alpha',0,'qlim', deg2rad([-360 360]));
-            link(4) = Link('d',0,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]));
-            link(5) = Link('d',0,'a',-0.115,'alpha',0,'qlim',deg2rad([-360 360]));
-            link(6) = Link('d',0.795,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]));
-            link(7) = Link('d',0,'a',0,'alpha',pi/2,'qlim',deg2rad([-360,360]));
-            link(8) = Link('d',0.165,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360,360]));
+            link(2) = Link('d',0.455,'a',0.150,'alpha',-pi/2,'qlim',[-180 180]*pi/180);
+            link(3) = Link('d',0,'a',0.900,'alpha',0,'qlim', [-95 155]*pi/180,'offset', -pi/2);
+            link(4) = Link('d',0,'a',-0.115,'alpha',pi/2,'qlim',[-180 75]*pi/180,'offset',pi);
+            link(5) = Link('d',0.795,'a',0,'alpha',pi/2,'qlim',[-400 400]*pi/180);
+            link(6) = Link('d',0,'a',0,'alpha',pi/2,'qlim',[-120 120]*pi/180,'offset',pi);
+            link(7) = Link('d',0.165,'a',0,'alpha',-pi/2,'qlim',[-400 400]*pi/180);
 
-%%ABB-IRB-2600/12's joint limits ADDED
             % Incorporate joint limits
-            % link(1).qlim = [0.8 0.01];
-            % link(2).qlim = [-180 180]*pi/180;
-            % link(3).qlim = [-95 155]*pi/180;
-            % link(4).qlim = [-180 75]*pi/180;
-            % link(5).qlim = [-400 400]*pi/180;
-            % link(6).qlim = [-120 120]*pi/180;
-            % link(7).qlim = [-400 400]*pi/180;
-
-%%add any offsetting here
-            % link(3).offset = -pi/2;
-            % link(5).offset = -pi/2;
-           
+            link(1).qlim = [0.8 0.01];
+        
             self.model = SerialLink(link,'name',self.name);
         end
      
