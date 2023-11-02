a8dc92b6eeb9da9b14d169dbf33f2616d0d8d15b
ElisaZ <73983562+elissssaz@users.noreply.github.com>
2023-11-02 19:38:04 +1100

    ABB complete

--- @LinearABB/LinearABB.m
+++ @LinearABB/LinearABB.m
@@ -14,7 +14,8 @@ function self = LinearABB(baseTr)
             end
             self.model.base = self.model.base.T * baseTr * trotx(pi/2) * troty(pi/2);
 
-            self.PlotAndColourRobot();         
+            self.PlotAndColourRobot(); 
+            
         end
 
 %% Create the robot model
@@ -22,29 +23,20 @@ function self = LinearABB(baseTr)
             % Create the UR5 model mounted on a linear rail
             link(1) = Link([pi     0       0       pi/2    1]); % PRISMATIC Link - linear rail
 %%ABB-IRB-2600/12's dh parameters ADDED
-            link(2) = Link('d',455,'a',0,'alpha',0,'qlim',deg2rad([-360 360]), 'offset',0);
-            link(3) = Link('d',0,'a',150,'alpha',pi/2,'qlim', deg2rad([-360 360]), 'offset',0);
-            link(4) = Link('d',0,'a',900,'alpha',0,'qlim', deg2rad([-360 360]), 'offset', 0);
-            link(5) = Link('d',795,'a',115,'alpha',pi/2,'qlim',deg2rad([-360 360]),'offset', 0);
-            link(6) = Link('d',0,'a',0,'alpha',pi/2,'qlim',deg2rad([-360,360]), 'offset',0);
-            link(7) = Link('d',165,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360,360]), 'offset', 0);
-	%link(8) = Link('d',_,'a',_,'alpha',_,'qlim',deg2rad([-360,360]), 'offset', 0)
+            link(2) = Link('d',0.455,'a',0.150,'alpha',-pi/2,'qlim',[-180 180]*pi/180);
+            link(3) = Link('d',0,'a',0.900,'alpha',0,'qlim', [-95 155]*pi/180,'offset', -pi/2);
+            link(4) = Link('d',0,'a',-0.115,'alpha',pi/2,'qlim',[-180 75]*pi/180,'offset',pi);
+            link(5) = Link('d',0.795,'a',0,'alpha',pi/2,'qlim',[-400 400]*pi/180);
+            link(6) = Link('d',0,'a',0,'alpha',pi/2,'qlim',[-120 120]*pi/180,'offset',pi);
+            link(7) = Link('d',0.165,'a',0,'alpha',-pi/2,'qlim',[-400 400]*pi/180,'offset',pi/2);
 
-%%ABB-IRB-2600/12's joint limits ADDED
             % Incorporate joint limits
-            link(1).qlim = [-0.8 -0.01];
-            link(2).qlim = [-180 180]*pi/180;
-            link(3).qlim = [-95 155]*pi/180;
-            link(4).qlim = [-180 75]*pi/180;
-            link(5).qlim = [-400 400]*pi/180;
-            link(6).qlim = [-120 120]*pi/180;
-            link(7).qlim = [-400 400]*pi/180;
-
-%%add any offsetting here
-            %link(3).offset = -pi/2;
-            %link(5).offset = -pi/2;
+            link(1).qlim = [-3.8   -0.01];
             
+        
             self.model = SerialLink(link,'name',self.name);
+
+            % self.model.teach()
         end
      
     end
