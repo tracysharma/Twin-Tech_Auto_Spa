c5d1c26ff237c08d66013a071a6c1c796413e068
ElisaZ <73983562+elissssaz@users.noreply.github.com>
2023-11-08 21:45:49 +1100

    Delete ArduinoCode.m

--- ArduinoCode.m
+++
@@ -1,4 +0,0 @@
-% Write 'serialportlist' in command window
-% then write 'arduinoObject = arduino('COM3'<-- Depends on COM,'Nano3')'
-% then write 'readDigitalPin( arduinoObject,'D6') whatever the button is on
-% arduino
